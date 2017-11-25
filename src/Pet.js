d033415408a0417078bd373e9e17fdf438b3490b
Vladyslav Nesterenko <easterenko@gmail.com>
2017-11-25 21:08:05 +0200

    Added GIFs and improved logic

--- src/Pet.js
+++ src/Pet.js
@@ -18,6 +18,7 @@ export default class Pet extends React.Component {
   constructor(props) {
     super(props)
 
+    this.shouldRender = true
     this.state = {
       indicators: {
         experience: {
@@ -55,6 +56,7 @@ export default class Pet extends React.Component {
     this.actions = {
       learnNew: {
         name: 'Learn new tech',
+        gif: require('./assets/gifs/learn.gif'),
         action: () => {
           this.setState(prevState => {
             return {
@@ -70,6 +72,7 @@ export default class Pet extends React.Component {
       },
       goToConf: {
         name: 'Go to a conference',
+        gif: require('./assets/gifs/conference.gif'),
         action: () => {
           this.setState(prevState => {
             return {
@@ -78,7 +81,6 @@ export default class Pet extends React.Component {
                experience: {value: prevState.indicators.experience.value + 0.5},
                productivity: {value: prevState.indicators.productivity.value + 1},
                majesty: {value: prevState.indicators.majesty.value + 1},
-               satisfaction: {value: prevState.indicators.satisfaction.value + 25},
                satiety: {value: prevState.indicators.satiety.value - 20},
               })
             }
@@ -87,10 +89,12 @@ export default class Pet extends React.Component {
         },
       petProject: {
         name: 'Work on a pet project',
+        gif: require('./assets/gifs/petProject.gif'),
         action: () => {
            this.setState(prevState => {
             return {
               indicators:Object.assign(this.state.indicators,{
+               satiety: {value: Math.round(prevState.indicators.satiety.value / 2)},
                experience: {value: prevState.indicators.experience.value + 0.5},
                majesty: {value: prevState.indicators.majesty.value + 0.25},
                satisfaction: {value: prevState.indicators.satisfaction.value + 15},
@@ -99,26 +103,15 @@ export default class Pet extends React.Component {
           })
        }
       },
-      gym: {
-        name: 'Go to gym',
-        action: () => {
-          this.setState(prevState => {
-            return {
-              indicators:Object.assign(this.state.indicators,{
-               satiety: {value: prevState.indicators.satiety.value - 20},
-               productivity: {value: Math.round(prevState.indicators.productivity.value / 1.45)},
-              })
-            }
-          })
-        }
-      },
       eat: {
         name: 'Eat!',
+        gif: require('./assets/gifs/eat.gif'),
         action: () => {
           this.setState(prevState => {
             return {
               indicators:Object.assign(this.state.indicators,{
-               satiety: {value: prevState.indicators.satiety.value + 20},
+               health: {value: prevState.indicators.health.value + 30},
+               satiety: {value: prevState.indicators.satiety.value + 30},
                money: {value: prevState.indicators.money.value - 50},
               })
             }
@@ -127,12 +120,13 @@ export default class Pet extends React.Component {
       },
       freelance: {
         name: 'take a project on freelance',
+        gif: require('./assets/gifs/freelance.gif'),
         action: () => {
           this.setState(prevState => {
             return {
               indicators:Object.assign(this.state.indicators,{
                majesty: {value: prevState.indicators.majesty.value - 1},
-               money: {value: prevState.indicators.money.value + 100},
+               money: {value: prevState.indicators.money.value + prevState.indicators.salary.value*2},
                satisfaction: {value: prevState.indicators.satisfaction.value - 25},
               })
             }
@@ -141,15 +135,34 @@ export default class Pet extends React.Component {
       },
       overtime: {
         name: 'work overtime',
+        gif: require('./assets/gifs/overtime.gif'),
         action: () => {
           this.setState(prevState => {
             return {
               indicators:Object.assign(this.state.indicators,{
                experience: {value: prevState.indicators.experience.value + 2},
                majesty: {value: prevState.indicators.majesty.value + 2},
-               salary: {value: prevState.indicators.salary.value + 1},
+               salary: {value: prevState.indicators.salary.value + 10},
                satisfaction: {value: Math.round(prevState.indicators.satisfaction.value / 2)},
                satiety: {value: Math.round(prevState.indicators.satiety.value / 2)},
+               money: {value: prevState.indicators.money.value + prevState.indicators.salary.value*2},
+               health: {value: prevState.indicators.health.value - 20},
+              })
+            }
+          })
+        }
+      },
+      coffee: {
+        name: 'Have some coffee',
+        gif: require('./assets/gifs/coffee.gif'),
+        action: () => {
+          this.setState(prevState => {
+            return {
+              indicators:Object.assign(this.state.indicators,{
+               money: {value: prevState.indicators.money.value / 2},
+               satisfaction: {value: prevState.indicators.satisfaction.value + 60},
+               health: {value: prevState.indicators.health.value - 10},
+               productivity: {value: prevState.indicators.productivity.value * 2},
 
               })
             }
@@ -159,10 +172,13 @@ export default class Pet extends React.Component {
       },
       leave: {
         name: 'You left programming because of',
+        // gif: require('./assets/gifs/leave.gif'),
         action: reason => `${this.name} ${reason}`
       }
     }
+
   }
+
 
   renderData() {
     return (<Grid xs={12} md={6} lg={4} item={true} id="indicators">
@@ -174,7 +190,7 @@ export default class Pet extends React.Component {
               <ListItem key={index} className={this.state.indicators[indicator].value < 25 ? 'danger' : 'ok'}>
                 <Typography type={'display1'} color="default" paragraph={true}>
                   {indicator} {this.state.indicators[indicator].value}
-                  {index < 4 ? null
+                  {index <= 4 ? null
                    : <LinearProgress mode="determinate" value={this.state.indicators[indicator].value} />}
                 </Typography>
               </ListItem>
@@ -196,21 +212,32 @@ export default class Pet extends React.Component {
           <Typography type={'display3'} color="primary" align="center" paragraph={true}>
             You
           </Typography>
-            <img style={{width: '100%'}} src={require(`./assets/${this.getLevelImage()}`)} />
+            <img style={{width: '100%'}} src={this.state.actionGif || require(`./assets/${this.getLevelImage()}`)} />
 
         </Grid>)
   }
 
+  doAction(action) {
+    // console.log(this.state)
+    action.action()
+    this.setState(prevState => Object.assign(prevState, {
+      actionGif: action.gif
+    }))
+
+  }
+
   renderActions() {
+    var actions = this.actions
     return (<Grid xs={12} md={6} lg={4} item={true} id="actions">
           <Typography type={'display2'} color="primary" paragraph={true}>
             Actions
           </Typography>
           <List>
-            {Object.keys(this.actions).slice(0,-1).map((action, index) => (
+            {Object.keys(actions).slice(0,-1).map((action, index) => (
               <ListItem key={index}>
-               <Button raised color="primary" onClick={this.actions[action].action}>
-                {this.actions[action].name}
+               <Button raised color="primary"
+                onClick={this.doAction.bind(this,actions[action])}>
+                {actions[action].name}
                </Button>
               </ListItem>
               ))}
